76a8cc3aba6edcce6056fa848a9ebe4b36308d3b
Esubalew Teshager <esuba419@gmail.com>
2026-02-04 01:06:07 +0300

    feat: enable CORS support in the server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from "../swagger.config"
 import dotenv from 'dotenv';
@@ -9,6 +10,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 
